41b983ff8ebc41f96e41aac002cf93abfe8a9fd1
Oscar Landa <blackmonlol99@gmail.com>
2023-01-17 14:09:51 -0600

    Astro deply done

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://1-Oscar-1.github.io',
+  base: '/Astro-Landing',
 });
